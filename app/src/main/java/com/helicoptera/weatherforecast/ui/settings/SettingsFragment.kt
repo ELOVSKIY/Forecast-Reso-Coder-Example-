b445de801d734900a0d9bc91a6a91c6f3142d797
Stas Elovsky <stas.elovsky@gmail.com>
2019-10-16 15:31:56 +0300

    UI

---
+++ app/src/main/java/com/helicoptera/weatherforecast/ui/settings/SettingsFragment.kt
@@ -0,0 +1,8 @@
+package com.helicoptera.weatherforecast.ui.settings
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+
+class SettingsFragment : Fragment(){
+
+}
---
+++ app/src/main/java/com/helicoptera/weatherforecast/ui/weather/future/detail/FutureDetailViewModel.kt
@@ -0,0 +1,7 @@
+package com.helicoptera.weatherforecast.ui.weather.future.detail
+
+import androidx.lifecycle.ViewModel
+
+class FutureDetailViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
